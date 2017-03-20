3972ea4623b375ffa3d7612780b244289caf3237
risca <risca@portatile>
2017-03-21 00:48:56 +0100

    added more infos

--- changelog/__init__.py
+++ changelog/__init__.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 """Show changelog and available versions for a given file"""
 import subprocess
+import re
 from sys import argv
 from os import path
 
@@ -24,13 +25,37 @@ def sha_exists(sha):
     test = run(command)
     return not test[2]
 
+def commit_date(commit):
+    """Return the author commit date"""
+    command = ['git', 'show', '-s', '--pretty=format:%aD', commit]
+    test = run(command)
+    return test[0]
+
 def all_sha_name(filepath):
     """Return all commit where the filepath has been changed"""
     info = run(['git', 'log', '--format="%H"', filepath])[0]
     commits = info.strip().replace('"', '').split('\n')
     return commits
 
+def size_from_sha_name(sha, human=False):
+    """Return the size embeded into the sha name"""
+    size = re.search('-s(\d*)--', sha).group(1)
+    if human:
+        return humansize(size)
+    else:
+        return size
 
+def humansize(nbytes):
+    """Return a human readable size"""
+    suffixes = ['B', 'K', 'M', 'G', 'T', 'P']
+    nbytes = float(nbytes)
+    if nbytes == 0: return '0B'
+    i = 0
+    while nbytes >= 1024 and i < len(suffixes)-1:
+        nbytes /= 1024.
+        i += 1
+    f = ('%.2f' % nbytes).rstrip('0').rstrip('.')
+    return '%s%s' % (f, suffixes[i])
 
 if __name__ == "__main__":
     """Usage:
@@ -43,5 +68,7 @@ if __name__ == "__main__":
     for commit in all_sha_name(PATH):
         sha = sha_name(commit, PATH)
         if sha_exists(sha):
-            print 'change at {} with {}'.format(commit, sha)
+            size = size_from_sha_name(sha, human=True)
+            date = commit_date(commit)
+            print 'change at {} with {}: {} [{}]'.format(date, size, sha, commit)
 
