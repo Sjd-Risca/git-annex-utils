651c45b90225e4fb9cbd8f9ca9e1fb931beb7805
risca <risca@portatile>
2017-03-20 23:58:34 +0100

    variable cleanup

--- changelog/__init__.py
+++ changelog/__init__.py
@@ -13,18 +13,18 @@ def run(command):
     out, err = exe.communicate()
     return [out, err, exe.returncode]
 
-def shaname(_commit, filepath):
+def sha_name(commit, filepath):
     """Return the sha of a certain filepath at a certain commit history"""
-    pointer = run(['git', 'show', '{0}:{1}'.format(_commit, filepath)])
+    pointer = run(['git', 'show', '{0}:{1}'.format(commit, filepath)])
     return path.basename(pointer[0])
 
-def sha_exists(_sha):
+def sha_exists(sha):
     """Return true if the sha file exists on any reposiroty"""
-    command = ['git', 'annex', 'whereis', '--key', _sha]
+    command = ['git', 'annex', 'whereis', '--key', sha]
     test = run(command)
     return not test[2]
 
-def allshaname(filepath):
+def all_sha_name(filepath):
     """Return all commit where the filepath has been changed"""
     info = run(['git', 'log', '--format="%H"', filepath])[0]
     commits = info.strip().replace('"', '').split('\n')
@@ -40,8 +40,8 @@ if __name__ == "__main__":
     It will return all sha still available"""
     PATH = argv[1:][0]
     print PATH
-    for commit in allshaname(PATH):
-        sha = shaname(commit, PATH)
+    for commit in all_sha_name(PATH):
+        sha = sha_name(commit, PATH)
         if sha_exists(sha):
             print 'change at {} with {}'.format(commit, sha)
 
